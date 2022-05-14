824733cbaa948e21887c33e2507a750658705be8
ihabfallahy2 <ihab2000@hotmail.es>
2022-05-14 18:33:45 +0200

    pctools-18:33/14/05/2022

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,3 @@
-import {CartComponent} from '../../../../../Desktop/angular/02-03-2022/tienda-prueba/src/app/components/cart/cart.component';
-import {SignerComponent} from '../../../../../Desktop/angular/02-03-2022/tienda-prueba/src/app/components/signer/signer.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ContentComponent } from './components/content/content.component';
@@ -10,9 +8,7 @@ import { DetailsComponent } from './components/details/details.component';
 const routes: Routes = [
   {path:'',component: ContentComponent},
   {path:'content',component: ContentComponent},
-  {path:'signer',component: SignerComponent},
   {path:'results',component: ResultsComponent},
-  {path:'cart',component: CartComponent},
   {path:'terms',component: TermsComponent},
   {path:'details',component: DetailsComponent},
 ];
